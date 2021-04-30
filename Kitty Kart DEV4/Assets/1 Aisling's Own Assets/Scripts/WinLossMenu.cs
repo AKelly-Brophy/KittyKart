639fe85d4148a6fc3f53e57f2f1291dc451ad2c8
AKelly-Brophy <48012713+AKelly-Brophy@users.noreply.github.com>
2021-04-30 23:37:57 +0100

    Created a "You Win!" scene

---
+++ Kitty Kart DEV4/Assets/1 Aisling's Own Assets/Scripts/WinLossMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WinLossMenu : MonoBehaviour
+{
+    Scene scene;
+    public string MainGame, Track2;
+
+    void Start() {
+        scene = SceneManager.GetActiveScene();
+    }
+
+    public void Replay()
+    {
+        if(scene.name == MainGame ) {
+            SceneManager.LoadScene("MainGame");
+        } else {
+            SceneManager.LoadScene("Track2");
+        }
+    }
+
+
+    public void TrackSelect()
+    {
+        SceneManager.LoadScene("TrackSelect");
+    }
+
+    public void ExitToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
